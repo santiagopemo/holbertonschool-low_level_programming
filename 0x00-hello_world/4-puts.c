c3a0e15e76efbae259c21b901624c9cf1bedf4d2
santiagopemo <santiagopemo@gmail.com>
2020-02-05 21:00:46 +0000

    C exercises

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Use of puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
