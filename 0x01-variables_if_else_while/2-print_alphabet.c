77f54e0ba42201eb25cdfc9e749079beabe5f45c
santiagopemo <santiagopemo@gmail.com>
2020-02-06 19:50:13 +0000

    if else exercises

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - c program that prints the alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
