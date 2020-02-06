e0ed309a0042b291020520229e33901ce1c3c2e6
santiagopemo <santiagopemo@gmail.com>
2020-02-06 22:50:56 +0000

    if else exercises

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - c program that prints the alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i < 58; ++i)
+	{
+		for (j = 48; j < 58; ++j)
+		{
+			putchar(i);
+			putchar(j);
+			if (i < 57 || j < 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
