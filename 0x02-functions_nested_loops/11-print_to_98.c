90602954a32c6cf31b0b9c17a4485bcd9fba8673
santiagopemo <santiagopemo@gmail.com>
2020-02-11 19:43:42 +0000

    function exercises

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print from n to 98
+ * @n: begin number
+ * Return: nothing
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i < 98)
+				printf(", ");
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i > 98)
+				printf(", ");
+		}
+	}
+	printf("\n");
+}
