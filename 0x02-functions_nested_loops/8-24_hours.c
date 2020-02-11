41e22dbe2058a25722fe75c68702adfb83eef7c4
santiagopemo <santiagopemo@gmail.com>
2020-02-11 15:02:07 +0000

    function exercises

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,30 +2,28 @@
 
 /**
  * jack_bauer - print 24
- * @n: number
+ *
  *
  * Return: nothing
  */
 void jack_bauer(void)
 {
-	int m1, m2, h1, h2;
+	int i, j, m1, m2, h1, h2;
 
-	for(h1 = 0; h1 <= 2; h1++)
+	for (i = 0; i < 24; i++)
 	{
-		for(h2 = 0; h2 <= 9; h2++)
+		h1 = i / 10;
+		h2 = i % 10;
+		for (j = 0; j < 60; j++)
 		{
-			for(m1 = 0; m1 <= 5; m1++)
-			{
-				for(m2 = 0; m2 <= 9; m2++)
-				{
-					_putchar(h1 + '0');
-					_putchar(h2 + '0');
-					_putchar(':');
-					_putchar(m1 + '0');
-					_putchar(m2 + '0');
-					_putchar('\n');
-				}
-			}
+			m1 = j / 10;
+			m2 = j % 10;
+			_putchar(h1 + '0');
+			_putchar(h2 + '0');
+			_putchar(':');
+			_putchar(m1 + '0');
+			_putchar(m2 + '0');
+			_putchar('\n');
 		}
 	}
 }
