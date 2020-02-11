e473ab9d8a4c8e6a3c62ccea80feeca21a5ae137
santiagopemo <santiagopemo@gmail.com>
2020-02-11 14:19:50 +0000

    function exercises

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,12 +11,12 @@ int print_last_digit(int n)
 
 	if (n >= 0)
 	{
-		_putchar((n % 10) + '0');
+		_putchar((n % 10) + 48);
 		return (n % 10);
 	}
 	else
 	{
-		_putchar((-n % 10) + '0');
+		_putchar((-n % 10) + 48);
 		return (-n % 10);
 	}
 }
