22951e6b0047e6916dbcb02217e0e8b3d3c43db7
santiagopemo <santiagopemo@gmail.com>
2020-02-13 19:47:14 +0000

    more function exercises

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - prinat a diagonal
+ * @n: number of times
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+	
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= n; j++)
+			{
+				if (i == j)
+				{
+					_putchar(92);
+					_putchar('\n');
+					break;
+				}
+				else
+					_putchar(' ');
+			
+			}
+		}
+	}
+	else
+		_putchar('\n');
+	
+}
