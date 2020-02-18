fac3431d2f5b8ecf0a55afa68f0671170d760fd9
santiagopemo <santiagopemo@gmail.com>
2020-02-18 21:50:20 +0000

    pointers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * puts_half - puts from half
+ * @str: string
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int l;
+
+	for (l = 0; * (str + l); l++);
+	for (str += (l + 1) / 2; *str; str++)
+		_putchar (*str);
+	_putchar('\n');
+}
