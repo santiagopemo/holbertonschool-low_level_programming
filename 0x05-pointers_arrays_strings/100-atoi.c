429b5df60f4742813b3c3d4f6319857288a50beb
santiagopemo <santiagopemo@gmail.com>
2020-02-20 02:14:18 +0000

    pointers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,25 +14,28 @@ int _atoi(char *s)
 	teens = 10;
 	sign = 1;
 	i = 0;
-	while (*(s + i) != '\0')
+	if (s)
 	{
-		if (*(s + i) == '-')
-			sign *= -1;
-		if (*(s + i) >= '0' && *(s + i) <= '9' && find_num == 'N')
+		while (*(s + i) != '\0')
 		{
-			find_num = 'Y';
-			my_num = *(s + i) - '0';
+			if (*(s + i) == '-')
+				sign *= -1;
+			if (*(s + i) >= '0' && *(s + i) <= '9' && find_num == 'N')
+			{
+				find_num = 'Y';
+				my_num = *(s + i) - '0';
+			}
+			else if (*(s + i) >= '0' && *(s + i) <= '9' && find_num == 'Y')
+			{
+				my_num *= teens;
+				my_num += (*(s + i) - '0');
+			}
+			else if ((*(s + i) < '0' || *(s + i) > '9') && find_num == 'Y')
+			{
+				break;
+			}
+			i++;
 		}
-		else if (*(s + i) >= '0' && *(s + i) <= '9' && find_num == 'Y')
-		{
-			my_num *= teens;
-			my_num += (*(s + i) - '0');
-		}
-		else if ((*(s + i) < '0' || *(s + i) > '9') && find_num == 'Y')
-		{
-			break;
-		}
-		i++;
 	}
 	return (sign * my_num);
 }
--- 0x05-pointers_arrays_strings/100-main.c
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -10,6 +10,8 @@ int main(void)
 {
     int n;
 
+    n = _atoi("aaaa");
+    printf("%d\n", n);
     n = _atoi("98");
     printf("%d\n", n);
     n = _atoi("-402");
