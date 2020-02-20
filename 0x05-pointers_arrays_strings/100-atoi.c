f2e3bdd2b32484494e7150e595bc6bcd3f8cdea7
santiagopemo <santiagopemo@gmail.com>
2020-02-20 02:01:20 +0000

    pointers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
  * _atoi - find numbers
  * @s:char
@@ -10,29 +10,28 @@ int _atoi(char *s)
 	int i, sign, my_num, teens;
 	char find_num = 'N';
 
-	teens = 1;
+	teens = 10;
 	sign = 1;
 	i = 0;
-	while (*(s + 1) != '\0')
+	while (*(s + i) != '\0')
 	{
 		if (*(s + i) == '-')
 			sign *= -1;
 		if (*(s + i) >= '0' && *(s + i) <= '9' && find_num == 'N')
 		{
 			find_num = 'Y';
-			/*first_num = i;*/
 			my_num = *(s + i) - '0';
-			teens = 10;
 		}
-		if (*(s + i) >= '0' && *(s + i) <= '9' && find_num == 'Y')
+		else if (*(s + i) >= '0' && *(s + i) <= '9' && find_num == 'Y')
 		{
-			/*last_num = i;*/
 			my_num *= teens;
 			my_num += (*(s + i) - '0');
-			teens *= 10;
 		}
-		if (*(s + i) < '0' && *(s + i) > '9' && find_num == 'Y')
+		else if ((*(s + i) < '0' || *(s + i) > '9') && find_num == 'Y')
+		{
 			break;
+		}
+		i++;
 	}
-	return (my_num);
+	return (sign * my_num);
 }
