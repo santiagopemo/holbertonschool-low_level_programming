8092a747105e929051172516c951e3b503152087
santiagopemo <santiagopemo@gmail.com>
2020-02-20 19:34:27 +0000

    more pointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,31 +2,18 @@
 
 /**
  * _strcmp - compare 2 strings
- * s1: first string
- * s2: second string
+ * @s1: first string
+ * @s2: second string
  * Return: int
  */
 int _strcmp(char *s1, char *s2)
 {
-	int sum_s1, sum_s2, res;
-
-	sum_s1 = 0;
-	sum_s2 = 0;
-	while (*s1)
+	while (*s1 && *s2)
 	{
-		sum_s1 += *s1;
+		if (*s1 != *s2)
+			return (*s1 - *s2);
 		s1++;
-	}
-	while (*s2)
-	{
-		sum_s2 += *s2;
 		s2++;
 	}
-	if (s1 < s2)
-		res = -15;
-	else if (s1 == s2)
-		res = 0;
-	else
-		res = 15;
-	return (res);
+	return (0);
 }
