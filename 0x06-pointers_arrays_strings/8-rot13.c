a35804b8d1e3435244780ad2bfbecc93b778e7b2
santiagopemo <santiagopemo@gmail.com>
2020-02-21 00:24:14 +0000

    more pointers

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * leet - encode string
+ * @c: char
+ * Return: char
+ */
+char *rot13(char *c)
+{
+	int i, j;
+	char l[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char n[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; *(c + i); i++)
+	{
+		for (j = 0; *(l + j); j++)
+		{
+			if (*(c + i) == *(l + j))
+			{
+				*(c + i) = *(n + j);
+			}
+		}
+	}
+	return (c);
+}
