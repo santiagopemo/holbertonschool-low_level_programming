92e422d003cf8007698a6d96abdbe2b701c36725
santiagopemo <santiagopemo@gmail.com>
2020-02-26 19:12:38 +0000

    recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string
+ * Return: lenght
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (_strlen_recursion(s + 1) + 1);
+	}
+	return (0);
+}
