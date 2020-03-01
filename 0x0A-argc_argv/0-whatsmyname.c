5a2ed583331677ce1e60317d709c7bf89824648e
santiagopemo <santiagopemo@gmail.com>
2020-03-01 15:55:26 +0000

    arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: count of arguments in main function
+ * @argv: contains the strings of arguments in main function
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	UNUSED(argc);
+	printf("%s\n", *argv);
+	return (0);
+}
