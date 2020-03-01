457f6d40b59d40967b8d7a111df37e5d8bda546b
santiagopemo <santiagopemo@gmail.com>
2020-03-01 19:35:51 +0000

    arguments

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -19,24 +19,22 @@ int main(int argc, char **argv)
 		return (1);
 	}
 	rest = atoi(argv[1]);
-	if (rest < 1)
+	if (rest > 0)
 	{
-		printf("Error\n");
-		return (1);
-	}
-	while (rest)
-	{
-		if (rest >= 25)
-			rest -= 25;
-		else if (rest >= 10)
-			rest -= 10;
-		else if (rest >= 5)
-			rest -= 5;
-		else if (rest >= 2)
-			rest -= 2;
-		else if (rest >= 1)
-			rest -= 1;
-		coins += 1;
+		while (rest)
+		{
+			if (rest >= 25)
+				rest -= 25;
+			else if (rest >= 10)
+				rest -= 10;
+			else if (rest >= 5)
+				rest -= 5;
+			else if (rest >= 2)
+				rest -= 2;
+			else if (rest >= 1)
+				rest -= 1;
+			coins += 1;
+		}
 	}
 	printf("%d\n", coins);
 	return (0);
