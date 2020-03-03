4bdf44a74bb93402c8ed6c6e1f6cfab95c0072a2
santiagopemo <santiagopemo@gmail.com>
2020-03-03 21:35:44 +0000

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,26 +2,27 @@
 
 /**
  * _strdup - function that returns a pointer to a newly allocated space
- *@str: string
+ * @str: string
  *
  * Return: pointer to string
  */
 char *_strdup(char *str)
 {
 	char *a;
-	int size;
+	unsigned int size;
 
 	size = 0;
 	if (str)
 	{
 		while (*(str + size))
 			size++;
+		size++;
 		a = malloc(sizeof(char) * size);
 		if (a)
 		{
 			while (size--)
 			{
-			*(a + size) = *(str + size);
+				*(a + size) = *(str + size);
 			}
 			return (a);
 		}
