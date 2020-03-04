407d35cab039a5ff3ca829c0e08b064dd2b12133
santiagopemo <santiagopemo@gmail.com>
2020-03-04 02:03:30 +0000

    malloc

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * argstostr -  function that concatenates all the arguments of your program
+ * @ac: count 
+ * @av: array
+ * Return: char
+ */
+char *argstostr(int ac, char **av)
+{
+
+}
