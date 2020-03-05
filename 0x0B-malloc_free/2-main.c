52c37182d227ec65e5aa54a3cdb2436c0c7b6e81
santiagopemo <santiagopemo@gmail.com>
2020-03-05 18:22:36 +0000

    more alloc

--- 0x0B-malloc_free/2-main.c
+++ 0x0B-malloc_free/2-main.c
@@ -11,18 +11,10 @@ int main(void)
 {
     char *s;
 
-    s = _strdup("Holberton");
+    s = str_concat("Betty ", "Holberton");
     if (s == NULL)
     {
-        printf("failed to allocate memory\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    s = _strdup(NULL);
-    if (s == NULL)
-    {
-        printf("failed to allocate memory\n");
+        printf("failed\n");
         return (1);
     }
     printf("%s\n", s);
