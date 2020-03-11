1cfef7c4a5e077d98798672e82fd7c531bfa874f
santiagopemo <santiagopemo@gmail.com>
2020-03-11 22:05:11 +0000

    function pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,72 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * op_add - sum
+ * @a: num1
+ * @b: num2
+ *
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference
+ * @a: num1
+ * @b: num2
+ *
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product
+ * @a: num1
+ * @b: num2
+ *
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: num1
+ * @b: num2
+ *
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder of the division
+ * @a: num1
+ * @b: num2
+ *
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
