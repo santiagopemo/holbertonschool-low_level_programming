192e08edd60e1f0d23055a14e27034777b936a45
santiagopemo <santiagopemo@gmail.com>
2020-03-19 21:37:56 +0000

    task 2 added

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -21,7 +21,10 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	new->str = strdup(str);
 	if (new->str == NULL)
+	{
+		free(new);
 		return (NULL);
+	}
 	new->len = len;
 	new->next = *head;
 	*head = new;
