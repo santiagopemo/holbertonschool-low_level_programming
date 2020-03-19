2020410f33b8d8611eee1b8886b284222cc3e309
santiagopemo <santiagopemo@gmail.com>
2020-03-19 21:33:11 +0000

    task 2 added

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_node -  function that adds a new node at the beginning of a list_t list
+ * @head: head list pointer
+ * @str: string
+ *
+ * Return: pointer to new node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	int len = 0;
+	list_t *new;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[++len])
+		continue;
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = len;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
