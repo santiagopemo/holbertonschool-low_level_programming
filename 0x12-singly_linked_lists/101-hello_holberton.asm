c7cb7eff428cd745175f92fbd8478d4df9b8f5e3
santiagopemo <santiagopemo@gmail.com>
2020-03-20 01:53:19 +0000

    adding task 101

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+SECTION .data
+msg:      db "Hello, Holberton", 0
+fmt:      db "%s", 10, 0
+
+SECTION .text
+extern printf
+global main
+main:
+
+mov esi, msg    ; 64-bit Direccion comienzo de la cadena
+mov edi, fmt    ; Formato de la cadena
+mov eax, 0      ; printf is varargs,  EAX cuenta
+
+;el numero de argumentos no enteros pasados
+call printf
+
+mov ebx, 0      ; normal-exit code
+mov eax, 1      ; process-termination service
+int 0x80        ; linux kernel service
