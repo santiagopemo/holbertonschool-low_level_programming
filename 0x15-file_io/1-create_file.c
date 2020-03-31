e0ad5ad5d0057a5a5ec232848a2fd1c4ef43625f
santiagopemo <santiagopemo@gmail.com>
2020-03-31 21:05:41 +0000

    ading task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * create_file - function that creates a file
+ * @filename: name of the file to create
+ * @text_content: text content
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, t_len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	while (*(text_content + t_len))
+		t_len++;
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+	write(fd, text_content, t_len);
+	close(fd);
+	return (1);
+}
