4497e96d8f9d69cc9ebe38841c5a00b33ce6d646
santiagopemo <santiagopemo@gmail.com>
2020-07-04 00:11:51 +0000

    added task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,26 +8,13 @@ def island_perimeter(grid):
     """
     Returns the perimeter of the island described in grid
     """
-    max_h = -1
-    min_h = len(grid[0])
-    min_v = len(grid)
-    max_v = -1
+    p = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
-            if grid[i][j] == 1 and j < min_h:
-                min_h = j
-            if grid[i][j] == 1 and j > max_h:
-                max_h = j
-    for i in range(len(grid[0])):
-        for j in range(len(grid)):
-            if grid[j][i] == 1 and j < min_v:
-                min_v = j
-            if grid[j][i] == 1 and j > max_v:
-                max_v = j
-    if max_h == -1 or max_v == -1:
-        return 0
-    # print("max_h = {}   min_h = {}".format(max_h, min_h))
-    # print("max_v = {}   min_v = {}".format(max_v, min_v))
-    h = max_h - min_h + 1
-    v = max_v - min_v + 1
-    return h + h + v + v
+            if grid[i][j] == 1:
+                p += 4
+                if  i > 0 and grid[i - 1][j] == 1: 
+                    p -= 2
+                if  j > 0 and grid[i][j - 1] == 1: 
+                    p -= 2
+    return p
