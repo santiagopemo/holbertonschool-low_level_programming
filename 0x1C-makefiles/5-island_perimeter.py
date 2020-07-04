0e407346b802037cf4015d35725f3502bb90a7cf
santiagopemo <santiagopemo@gmail.com>
2020-07-04 00:12:58 +0000

    added task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,8 +13,8 @@ def island_perimeter(grid):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
                 p += 4
-                if  i > 0 and grid[i - 1][j] == 1: 
+                if i > 0 and grid[i - 1][j] == 1:
                     p -= 2
-                if  j > 0 and grid[i][j - 1] == 1: 
+                if j > 0 and grid[i][j - 1] == 1:
                     p -= 2
     return p
