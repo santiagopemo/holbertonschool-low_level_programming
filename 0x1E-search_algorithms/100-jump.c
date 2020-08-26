5064bf323667d4c4d015a85b22a1c1d542dcf60f
santiagopemo <santiagopemo@gmail.com>
2020-08-26 17:18:04 +0000

    corrected 100

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -27,11 +27,16 @@ int jump_search(int *array, size_t size, int value)
 	}
 	printf("Value checked array[%d] = [%d]\n", i, array[i]);
 	printf("Value found between indexes [%d] and [%d]\n", i, m);
-	for (; i <= m && i < n; i++)
+	for (; i < m && i < n; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
+	}
+	if (array[i] == value)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		return (i);
 	}
 	return (-1);
 }
